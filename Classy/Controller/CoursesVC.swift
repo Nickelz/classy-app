dd491307f499b28f0dd092782c5a4c73094e9eb9
Nickelz <xinickelz@gmail.com>
2019-06-26 21:55:55 +0300

    Added the courses page with the exclusion of the functionality. So basically I've only built the design of the view.

--- Classy/Controller/CoursesVC.swift
+++ Classy/Controller/CoursesVC.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class CoursesVC: UIViewController {
+	
+	// Components
+	var TitleBar: UIView!
+	var TitleLabel: UILabel!
+	var EditButton: UIButton!
+	var AddButton: UIButton!
+	var CoursesTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,16 +24,121 @@ class CoursesVC: UIViewController {
     }
     
     func createUserInterface() {
-        let label: UILabel = {
-            let label = UILabel()
-            label.frame = CGRect(x: 10, y: 10, width: 100, height: 75)
-            label.text = "Courses"
-            label.textColor = .black
-            
-            return label
-        }()
-        
-        self.view.addSubview(label)
+		let superView = self.view!
+		
+		TitleBar = {
+			let view = UIView()
+			
+			view.backgroundColor = .white
+			view.dropShadow(shadowRadius: 8.0, shadowOpacity: 1.0, shadowOffset: CGSize.zero, shadowColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.08))
+			view.layer.cornerRadius = 8
+			view.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+			superView.addSubview(view)
+			
+			view.snp.makeConstraints({ (make) in
+				make.top.equalToSuperview()
+				make.right.equalToSuperview()
+				make.left.equalToSuperview()
+				make.height.equalTo(91)
+			})
+			
+			return view
+		}()
+		
+		TitleLabel = {
+			let label = UILabel()
+			
+			TitleBar.addSubview(label)
+			
+			// Constraints
+			label.snp.makeConstraints({ (make) in
+				make.centerX.equalToSuperview()
+				make.bottom.equalToSuperview().offset(-15)
+			})
+			
+			// Customization
+			label.text = "Courses"
+			label.font = UIFont(name: "MavenPro-Bold", size: 19)
+			label.textColor = .black
+			
+			return label
+		}()
+		
+		EditButton = {
+			let button = UIButton()
+			
+			TitleBar.addSubview(button)
+			
+			// Constraints
+			button.snp.makeConstraints({ (make) in
+				make.centerY.equalTo(TitleLabel)
+				make.left.equalToSuperview().offset(20)
+			})
+			
+			// Customization
+			let icon = UIImage(named: "edit")
+			button.setImage(icon, for: .normal)
+			button.imageView?.contentMode = .scaleAspectFit
+			
+			return button
+		}()
+		
+		AddButton = {
+			let button = UIButton()
+			
+			TitleBar.addSubview(button)
+			
+			button.snp.makeConstraints({ (make) in
+				make.centerY.equalTo(TitleLabel)
+				make.right.equalToSuperview().offset(-20)
+			})
+			
+			let icon = UIImage(named: "add")
+			button.setImage(icon, for: .normal)
+			button.imageView?.contentMode = .scaleAspectFit
+			
+			return button
+		}()
+		
+		CoursesTableView = {
+			let tableView = UITableView()
+			
+			tableView.dataSource = self
+			tableView.delegate = self
+			
+			tableView.register(CourseCell.self, forCellReuseIdentifier: "Courses Cell")
+			
+			tableView.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1.0)
+			tableView.rowHeight = 80.0
+			tableView.separatorStyle = .none
+			
+			superView.addSubview(tableView)
+			
+			tableView.snp.makeConstraints({ (make) in
+				make.top.equalTo(TitleBar.snp.bottom)
+				make.right.equalToSuperview()
+				make.left.equalToSuperview()
+				make.bottom.equalToSuperview()
+			})
+			
+			return tableView
+		}()
     }
 
+}
+
+extension CoursesVC: UITableViewDelegate, UITableViewDataSource {
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 50
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let cell = tableView.dequeueReusableCell(withIdentifier: "Courses Cell", for: indexPath) as? CourseCell {
+			return cell
+		}
+		return UITableViewCell()
+	}
+	
+	
 }
